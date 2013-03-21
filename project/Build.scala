11707bcfb7e2839e29881235059ee7fa16d309bd
alwayslz <alwaysven@gmail.com>
2013-03-21 16:57:18 -0500

    add hello-world template

---
+++ project/Build.scala
@@ -0,0 +1,59 @@
+import sbt._
+
+import Keys._
+import AndroidKeys._
+import AndroidNdkKeys._
+
+object General {
+  // Some basic configuration
+  val settings = Defaults.defaultSettings ++ Seq (
+    name := "tipcal-scala",
+    version := "0.1",
+    versionCode := 0,
+    scalaVersion := "2.10.1",
+    platformName in Android := "android-17"
+  )
+
+  // Default Proguard settings
+  lazy val proguardSettings = inConfig(Android) (Seq (
+    useProguard := true,
+    proguardOptimizations += "-keep class luc.edu.tipcal.** { *; }"
+  ))
+
+  // Example NDK settings
+  lazy val ndkSettings = AndroidNdk.settings ++ inConfig(Android) (Seq(
+    jniClasses := Seq(),
+    javahOutputFile := Some(new File("native.h"))
+  ))
+
+  // Full Android settings
+  lazy val fullAndroidSettings =
+    General.settings ++
+    AndroidProject.androidSettings ++
+    TypedResources.settings ++
+    proguardSettings ++
+    AndroidManifestGenerator.settings ++
+    AndroidMarketPublish.settings ++ Seq (
+      keyalias in Android := "change-me",
+      libraryDependencies += "org.scalatest" %% "scalatest" % "1.9.1" % "test"
+    )
+}
+
+object AndroidBuild extends Build {
+  lazy val main = Project (
+    "main",
+    file("."),
+    settings = General.fullAndroidSettings ++ AndroidEclipseDefaults.settings
+  )
+
+  lazy val tests = Project (
+    "tests",
+    file("tests"),
+    settings = General.settings ++
+               AndroidEclipseDefaults.settings ++
+               AndroidTest.androidSettings ++
+               General.proguardSettings ++ Seq (
+      name := "tipcal-scalaTests"
+    )
+  ) dependsOn main
+}
